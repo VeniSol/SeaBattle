b8a0eac363cdf4d7f621b261df9757d87e8b0b3e
VeniSol <solodky2005@gmail.com>
2024-09-10 19:34:59 +0300

    bogdanSlomal

--- src/main/java/com/example/seabattle/Services/SeaService.java
+++ src/main/java/com/example/seabattle/Services/SeaService.java
@@ -1,13 +1,15 @@
 package com.example.seabattle.Services;
 
-import com.example.seabattle.Model.Fields;
 import com.example.seabattle.Model.Moves;
 import com.example.seabattle.Model.Player;
 import com.example.seabattle.Model.Ships;
 import com.example.seabattle.Repositories.MovesRepository;
+import com.example.seabattle.Repositories.PlayerRepository;
 import com.example.seabattle.Repositories.ShipsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 public class SeaService {
@@ -17,6 +19,8 @@ public class SeaService {
     MovesRepository movesRepository;
     public int[][] fieldForFirstPlayer = new int[10][10];
     public int[][] fieldForSecondPlayer = new int[10][10];
+    @Autowired
+    private PlayerRepository playerRepository;
 
     public boolean savingShipLocation(int shipSize, int orientation, int move_x, int move_y, Player player) {
         if (isActiveShip(shipSize, player)) {
@@ -64,13 +68,17 @@ public class SeaService {
         if (player.getPlayerId() == 1)
             if (fieldForSecondPlayer[move_x][move_y] == 1) {
                 fieldForSecondPlayer[move_x][move_y] = 4;
-                if (isShipSunk(move_x, move_y, player)) {
+                Player player2 = playerRepository.findByPlayerId(2);
+                if (isShipSunk(move_x, move_y, player2)) {
+                    PaintSunkShip(move_x, move_y, player2);
                 }
             } else fieldForSecondPlayer[move_x][move_y] = 3;
         if (player.getPlayerId() == 2)
             if (fieldForFirstPlayer[move_x][move_y] == 1) {
                 fieldForFirstPlayer[move_x][move_y] = 4;
-                if (isShipSunk(move_x, move_y, player)) {
+                Player player2 = playerRepository.findByPlayerId(1);
+                if (isShipSunk(move_x, move_y, player2)) {
+                    PaintSunkShip(move_x, move_y, player2);
                 }
             } else fieldForFirstPlayer[move_x][move_y] = 3;
         return true;
@@ -81,6 +89,9 @@ public class SeaService {
     }
 
     private boolean isShipSunk(int x, int y, Player player) {
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(player.getPlayerId());
         Ships ship = shipsRepository.findShipByCellAndPlayer(x, y, player);
         if (ship.getOrientation() == 1) {
             for (int i = ship.getStartX(); i <= ship.getEndX(); i++) {
@@ -103,15 +114,15 @@ public class SeaService {
         return true;
     }
 
-    public void PaintSunkShip(int x, int y, Player player) {
+    private void PaintSunkShip(int x, int y, Player player) {
         Ships ship = shipsRepository.findShipByCellAndPlayer(x, y, player);
         if (ship.getOrientation() == 1) {
             for (int i = ship.getStartX() - 1; i <= ship.getEndX() + 1; i++) {
                 for (int j = y - 1; j <= y + 1; j++) {
                     boolean checkX = i <= 10 && i >= 1;
                     boolean checkY = j <= 10 && j >= 1;
-                    boolean checkShip = shipsRepository.findShipByCellAndPlayer(i, j, player) != null;
-                    if (checkX && checkY && isShipSunk(x, y, player)) {
+                    boolean checkShip = shipsRepository.findShipByCellAndPlayer(i, j, player) == null;
+                    if (checkX && checkY && checkShip) {
                         if (player.getPlayerId() == 1) fieldForFirstPlayer[i][j] = 3;
                         else fieldForSecondPlayer[i][j] = 3;
                     }
@@ -122,14 +133,64 @@ public class SeaService {
                 for (int j = x - 1; j <= x + 1; j++) {
                     boolean checkY = i <= 10 && i >= 1;
                     boolean checkX = j <= 10 && j >= 1;
-                    boolean checkShip = shipsRepository.findShipByCellAndPlayer(j, i, player) != null;
-                    if (checkX && checkY && isShipSunk(x, y, player)) {
+                    boolean checkShip = shipsRepository.findShipByCellAndPlayer(j, i, player) == null;
+                    if (checkX && checkY && checkShip) {
                         if (player.getPlayerId() == 1) fieldForFirstPlayer[j][i] = 3;
                         else fieldForSecondPlayer[j][i] = 3;
                     }
                 }
             }
         }
+    }
+
+    public void randomShipGeneration() {
+        for (int i = 1; i <= 2; i++) {
+            Player player = playerRepository.findByPlayerId(i);
+            int[] freeShips = new int[]{4, 3, 3, 2, 2, 2, 1, 1, 1, 1};
+            int shipSet = 0;
+            int allShips = 10;
+            while (shipSet <= allShips) {
+                Random rnd = new Random();
+                int rndX = rnd.nextInt(10);
+                int rndY = rnd.nextInt(10);
+                int rndOrientation = rnd.nextInt(1, 3);
+                if (rndOrientation == 1 && rndX + freeShips[shipSet] <= 10 && checkOneCell(rndX, rndY, player)) {
+                    savingShipLocation(freeShips[shipSet], 1, rndX, rndY, player);
+                    shipSet++;
+
+                } else if (rndOrientation == 2 && rndY + freeShips[shipSet] <= 10 && checkOneCell(rndX, rndY, player)) {
+                    savingShipLocation(freeShips[shipSet], 2, rndX, rndY, player);
+                    shipSet++;
+                }
+
+            }
+        }
+    }
+
+    private boolean checkOneCell(int x, int y, Player player) {
+        Ships ship = shipsRepository.findShipByCellAndPlayer(x, y, player);
+        if (ship == null) return true;
+        if (ship.getOrientation() == 1) {
+            return presenceShip(x, y, player);
+        } else {
+            return presenceShip(y, x, player);
+        }
+    }
+
+    private boolean presenceShip(int a, int b, Player player) {
+        for (int i = a - 1; i <= a + 1; i++) {
+            for (int j = b - 1; j <= b + 1; j++) {
+                boolean checkX = i <= 10 && i >= 1;
+                boolean checkY = j <= 10 && j >= 1;
+                boolean checkShip = shipsRepository.findShipByCellAndPlayer(i, j, player) != null;
+                if (player.getPlayerId() == 1 && checkX && checkY && checkShip) {
+                    return false;
+                } else if (player.getPlayerId() == 2 && checkX && checkY && checkShip) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
 }
--- src/test/java/com/example/seabattle/SeaBattleApplicationTests.java
+++ src/test/java/com/example/seabattle/SeaBattleApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.seabattle;
 
 import com.example.seabattle.Model.Player;
+import com.example.seabattle.Repositories.MovesRepository;
 import com.example.seabattle.Repositories.PlayerRepository;
 import com.example.seabattle.Repositories.ShipsRepository;
 import com.example.seabattle.Services.SeaService;
@@ -19,6 +20,8 @@ class SeaBattleApplicationTests {
     PlayerRepository playerRepository;
     @Autowired
     ShipsRepository shipsRepository;
+    @Autowired
+    MovesRepository movesRepository;
     @Test
     void saveShip() {
         shipsRepository.deleteAll();
@@ -30,16 +33,17 @@ class SeaBattleApplicationTests {
     void setShip()
     {
         shipsRepository.deleteAll();
+        movesRepository.deleteAll();
+
         Player player = playerRepository.findByPlayerName("Cyxaruk");
         Player player2 = playerRepository.findByPlayerName("Boda");
-
-        seaService.savingShipLocation(1,2,1,1,player);
-        seaService.savingMovesLocation(1,1,player2);
-        seaService.PaintSunkShip(1,1,player);
+        seaService.savingShipLocation(2,2,6,6,player);
+        seaService.randomShipGeneration();
         for (int i=0;i<10;i++){
             for (int j=0;j<10;j++){
-                System.out.println(seaService.fieldForFirstPlayer[i][j]);
+                System.out.print(seaService.fieldForFirstPlayer[i][j]+" ");
             }
+            System.out.println();
         }
 
 
